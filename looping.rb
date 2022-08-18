06db4aae9817e41810312e2c40888460234b205d
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-08-18 08:16:58 +0300

    commit tree 8f5faef8898b7ec824f916d9f7c0bb3beaec0a25

--- looping.rb
+++ looping.rb
@@ -1,5 +1,9 @@
 def happy_new_year
   # your code here
+  (1..10).each do |i|
+    puts 11-i
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +21,18 @@ end
 
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |i|
+    puts fizzbuzz(i)
+  end
 end
 
 def reverse_string(str)
   # your code here
+  arr =[]
+  char = str.split("")
+  char.length.times do |i|
+    # puts i
+    arr.push(char[char.length - (i + 1)])
+  end
+  return arr.join("")
 end
